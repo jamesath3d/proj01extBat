4035bb7f1d75c54e55186c682dcd5685b78a4d2b
Your Name <you@example.com>
2022-01-14 10:58:06 -0500

    x

--- Extra_battery_package01/interupt_wdt.c
+++ Extra_battery_package01/interupt_wdt.c
@@ -2,21 +2,25 @@
 
 #include "main.h"
 
-__attribute__((interrupt(TIMER0_A0_VECTOR)))
+    __attribute__((interrupt(TIMER0_A0_VECTOR)))
 void _timer0_a0_isr(void)
 {
 
 } // timer0_a0_isr
 
-static uint8_t _wdt_cnt01 ;
-__attribute__((interrupt(WDT_VECTOR)))
+uint8_t _wdt_cnt01 ;
+    __attribute__((interrupt(WDT_VECTOR)))
 void _wdt_isr(void)
 {
     _wdt_cnt01 ++ ;
-    //if ( _wdt_cnt01 >= 32 ) {
-    if ( _wdt_cnt01 >= 64 ) {
+
+    if ( _wdt_cnt01 >> 5 ) {
         _wdt_cnt01 = 0  ;
-        _BIC_SR_IRQ(CPUOFF);
+        //_BIC_SR_IRQ(CPUOFF);
     }
 
+    SFRIFG1 &= ~WDTIFG;
+    //_BIC_SR_IRQ(LPM4_bits);
+    LPM4_EXIT;
 }
+
--- Extra_battery_package01/main.c
+++ Extra_battery_package01/main.c
@@ -5,44 +5,58 @@
 void main_test(void) {
 
     WDTCTL = WDT_MDLY_32 ;
-    WDTCTL = WDT_MDLY_8 ;
+    //WDTCTL = WDT_MDLY_8 ;
     SFRIE1   |= WDTIE ;
 
     P1DIR    |= BIT6 ;
     
     while( 1 ){
-        _BIS_SR( CPUOFF + GIE ) ;
-        P1OUT ^= BIT6 ;
-        //__delay_cycles(1000000);
-        //__delay_cycles(3000000);
+        //_BIS_SR( CPUOFF + GIE ) ;
+        //_BIS_SR( LPM0_bits + GIE ) ;
+        //_BIS_SR( LPM1_bits + GIE ) ;
+        //_BIS_SR( LPM2_bits + GIE ) ;
+        //_BIS_SR( LPM3_bits + GIE ) ;
+        _BIS_SR( LPM4_bits + GIE ) ;
+        // LPM0; // if no GIE, WDT don't work.
+
+        //P1OUT ^= BIT6 ;
+        extern uint8_t _wdt_cnt01 ;
+        if ( _wdt_cnt01 != 1 ) {
+            P1OUT |= BIT6 ;
+        } else {
+            P1OUT &= ~BIT6 ;
+        }
+
+            //__delay_cycles(1000000);
+            //__delay_cycles(3000000);
+        }
+
+    } // main_test
+
+
+    int main(void) {
+
+
+        uint8_t __ii ;
+        uint8_t __rt ;
+
+        main_test();
+
+        main_init();
+
+
+        // led_1234_init_test_loop();
+        // _uart_p1_5_tx_only_testloop();
+
+        led_1234_init_test_loop();
+
+        Delay_100ms(); // 0.1 second
+
+        while(1)
+        {
+            led_1234_init_test_once();
+        }
+
+
+
     }
-
-} // main_test
-
-
-int main(void) {
-
-
-    uint8_t __ii ;
-    uint8_t __rt ;
-
-    //main_test();
-
-    main_init();
-
-
-    // led_1234_init_test_loop();
-    // _uart_p1_5_tx_only_testloop();
-
-    led_1234_init_test_loop();
-
-    Delay_100ms(); // 0.1 second
-
-    while(1)
-    {
-        led_1234_init_test_once();
-    }
-
-
-
-}
