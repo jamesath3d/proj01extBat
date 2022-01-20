1f26f84cec77e776e03c4fbb16bb26c0ebdf9108
Your Name <you@example.com>
2022-01-20 17:36:08 -0500

    2022_0120_1736pm_08

--- Extra_battery_package01/led_34_follow_key1_t1_once.c
+++ Extra_battery_package01/led_34_follow_key1_t1_once.c
@@ -18,18 +18,18 @@ void led_34_follow_key1_t1_once(void){
 
     led34_follow_key1_once_idx ++;
     //if ( led34_follow_key1_once_idx >= 16 ) {
-    //    led34_follow_key1_once_idx = 0;
-    //if ( 1 == (led34_follow_key1_once_idx & 0x3 ) ) {
-    if ( 1 == 1 ) {
+    if ( 0 == (led34_follow_key1_once_idx & 0xF ) ) {
+    //if ( 0 == (led34_follow_key1_once_idx & 0x7 ) ) {
+    //if ( 0 == (led34_follow_key1_once_idx & 0x3 ) ) {
+    //if ( 1 == 1 ) {
 
         if ( _key_1_value_t1 ) {
             led_14_on();
+            led_14_off();
         } else {
             led_13_on();
+            led_13_off();
         }
-        //__delay_cycles(1); //__delay_cycles(1000);
-        led_13_off();
-        led_14_off();
     }
 
 } // led_34_follow_key1_t1_once
