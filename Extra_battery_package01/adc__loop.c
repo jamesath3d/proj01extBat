3593a94b9666f7766bce6085944c11c406ff1258
Your Name <you@example.com>
2022-02-02 23:41:19 -0500

    2022_0202_2341pm_19

--- Extra_battery_package01/adc__loop.c
+++ Extra_battery_package01/adc__loop.c
@@ -57,7 +57,9 @@ void adc__loop_once(void) {
             //adc_i32 = 3300 * adc_i16 / 0x3FF ; 
             adc_i32 = 3300 * adc_i16 / 0x400 ; 
             _uart_p1_5_tx_only_put_uint16( adc_i32 & 0xFFFF ) ; 
-            _uart_p1_5_tx_only_put_uint16d( 0xFFFF ) ; 
+            _uart_p1_5_tx_only_put_uint32d( 0xFFFFFFFF ) ; 
+            _uart_p1_5_tx_only_put_uint32d( 1 ) ; 
+            _uart_p1_5_tx_only_put_uint32d( 0 ) ; 
         } else {
             _uart_p1_5_tx_only_put_uint16( ADCMEM0 ) ; 
         }
