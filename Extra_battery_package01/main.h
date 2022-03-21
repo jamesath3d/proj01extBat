d6802173647bff8c4dc317646af9265bfc613777
Your Name <you@example.com>
2022-03-21 09:38:05 -0400

    2022_0321_0938am_05

--- Extra_battery_package01/main.h
+++ Extra_battery_package01/main.h
@@ -2,6 +2,8 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 
+#define Clk16384
+
 #include "_include_all.h"
 #include "led__init.h"
 #include "key__init.h"
@@ -10,14 +12,5 @@
 #include "vturn__init.h"
 #include "interupt_wdt.h"
 
-#define Clk16384
-
-#ifdef  Clk16384
-#define ClkInit()       _clk_init_16384()
-#define Delay_1000ms()  __delay_cycles(16384)  //  1000 ms
-#define Delay_100ms()   __delay_cycles(1638)   //  100  ms
-#define Delay_10ms()    __delay_cycles(163)    //  10   ms
-#define Delay_1ms()     __delay_cycles(16)     //  1    ms
-#endif
 
 #endif
---
+++ h01/_wdt_init.h
@@ -0,0 +1,1 @@
+../BlinkLED_MSP430FR2433_03/_wdt_init.h
