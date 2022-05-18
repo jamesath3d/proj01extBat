c26e5e47065fbe7e1ff85d9cc53599aa842630b4
Your Name <you@example.com>
2022-05-18 12:51:06 -0400

    2022_0518_1251pm_06

--- Extra_battery_package01/main.c
+++ Extra_battery_package01/main.c
@@ -36,6 +36,49 @@ void mainX6(void) {
         _WDT_wait_interrupt_LPM3 ;
     }
 } // mainX6
+void mainY1(void) {
+    //uint8_t __ii ;
+    uint8_t __tickCNT ;
+#define __keyActived_default 128
+    uint8_t __keyActivedCNT ;
+    __tickCNT = 0 ;
+    __keyActivedCNT = 0 ;
+
+    ledB = LedBr3 ; // for test only
+    while( 1 )
+    {
+        _WDT_wait_interrupt_LPM3 ;
+
+        __tickCNT ++ ;
+        if ( 0 == ( __tickCNT & 0xF ) ) {
+            //_uart_p1_5_tx_only_put_u8d( key_1_read() ) ;//             _READbit_(key_1) 
+            //_UART_P1_5_TX_PUT_CH(',');
+            _uart_p1_5_tx_only_put_u8d( __keyActivedCNT ) ;//             _READbit_(key_1) 
+            _UART_P1_5_TX_PUT_CH(',');
+
+            _uart_p1_5_tx_only_put_u8d( __tickCNT >> 4 );
+            _uart_p1_5_tx_only_put_rn();
+
+            // interupt_timer0_a0_isr
+
+            if ( (__tickCNT & 0x10) ) {
+                interupt_init_ccr1_for_led_off();
+            } else {
+                if ( __keyActivedCNT ) {
+                    interupt_init_ccr1_for_led_on();
+                }
+            }
+        }
+        if ( 0 == __keyActivedCNT ) {
+            if( 0 == key_1_read() ) {//             _READbit_(key_1) 
+                __keyActivedCNT = __keyActived_default ;
+            }
+        } else {
+            __keyActivedCNT -- ;
+        }
+
+    }
+} // mainY1
 
 int main(void) {
 
@@ -48,13 +91,16 @@ int main(void) {
     main_init();
     //led_1234_init_test_once_all_by_lpm(); // to indicate that the board is actived.
 
+    if(0) main_init_test2_test_flash_evry_gap();
+    if(0) main_init_test3_test_flash_evry_16_gap();
+
     if(0) mainX5();                         // interupt_timer0_a0_isr
-    if(1) mainX6();                         // interupt_timer0_a0_isr
+    if(0) mainX6();                         // interupt_timer0_a0_isr
+
+    if(1) mainY1();                         // interupt_timer0_a0_isr
 
     //_Y1( LED_on,        led13 );
     _WDT_wait_interrupt_LPM3_loop;
     //while(1){ _WDT_wait_interrupt_LPM0; }
 
-    main_init_test2_test_flash_evry_gap();
-    main_init_test3_test_flash_evry_16_gap();
 }
